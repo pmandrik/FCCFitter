41dc842c95569f2a09a7f77cda78f36dd8c8ba1e
clementhelsens <clement.helsens@cern.ch>
2018-01-22 15:29:17 +0100

    add and remove

---
+++ scripts/Zprime_ll/run_signi.sh
@@ -0,0 +1,5 @@
+python python/significance.py -f "config_FCC/Zprime_ll/fcc_v02/Zprime_ee_*TeV.config" -n Zprime_ee_fcc_v02
+python python/significance.py -f "config_FCC/Zprime_ll/fcc_v02/Zprime_mumu_*TeV.config" -n Zprime_mumu_fcc_v02
+python python/significance.py -f "config_FCC/Zprime_ll/fcc_v02/Zprime_ll_*TeV.config" -n Zprime_ll_fcc_v02 -c
+
+
