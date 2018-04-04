604199237680b5ef2ea9c9b810e76dd1ecca7116
clementhelsens <clement.helsens@cern.ch>
2018-04-04 14:52:55 +0200

    Q* script

--- scripts/Dijet/run_limitplots.sh
+++ scripts/Dijet/run_limitplots.sh
@@ -1,1 +1,1 @@
-python python/limitplot.py -f "Outputs/fcc_v02/Qstar/jj/Qstar_jj_*TeV/Limits/*" -n "Qstar_jj_fcc_v02" -p "Q* \rightarrow jj~~FCC" -s "pp_ExcitedQ_VALUETeV_qq"
+python python/limitplot.py -f "Outputs/fcc_v02/Qstar/jj/Qstar_jj_*TeV/Limits/*" -n "Qstar_jj_fcc_v02" -p "Q* \rightarrow jj" -s "p8_pp_ExcitedQ_VALUETeV_qq"
